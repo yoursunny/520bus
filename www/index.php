a57560360c9bb6a1d89e28b9cf9378fa284c8115
Junxiao Shi <git@mail1.yoursunny.com>
2017-01-15 01:29:27 -0700

    www: 520bus.site

---
+++ www/index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<title>520bus.site</title>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/F5gyqJEpTtk" frameborder="0" allowfullscreen></iframe>
+
+<h1>Alexa 520bus skill</h1>
+<p>Alexa, ask 520bus when does Sun Link depart Stone Ave?</p>
+
+<p><a href="https://hackarizona2017.hackerearth.com/sprints/hack-arizona-1/dashboard/yoursunny/submission/">submitted to Hack Arizona 2017</a></p>
